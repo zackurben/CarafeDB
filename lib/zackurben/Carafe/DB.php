1f14d22153fdfd62dcb0988b49e38a73ed92e0c0
Zack Urben <zackurben@gmail.com>
2014-11-23 15:23:28 -0600

    Adding the functionality to remove a row from the DB.

--- lib/zackurben/Carafe/DB.php
+++ lib/zackurben/Carafe/DB.php
@@ -45,6 +45,9 @@ class DB extends Core
      *
      * @return array $result_rows
      *   An array, with associative keys, of the resulting search.
+     *
+     * @throws InvalidColumnException
+     * @throws InvalidParameterException
      */
     public function select(array $param)
     {
@@ -102,6 +105,9 @@ class DB extends Core
      * @param array $row
      *   A new row of data to append to the Database object.
      *   (Note: The $row must match the existing rows in the DB.)
+     *
+     * @throws InvalidColumnException
+     * @throws InvalidParameterException
      */
     public function insert(array $row)
     {
@@ -129,5 +135,53 @@ class DB extends Core
         }
 
         file_put_contents($this->db, $temp, FILE_APPEND | LOCK_EX);
+    }
+
+    /**
+     * Remove all rows from the CarafeDB that matches the given row.
+     *
+     * @param array $row
+     *   The row to delete.
+     *
+     * @throws InvalidColumnException
+     * @throws InvalidParameterException
+     */
+    public function removeRow(array $row)
+    {
+        // Validate parameters type.
+        if (!is_array($row)) {
+            throw new InvalidParameterException(
+                'DB#removeRow()',
+                gettype($row),
+                'array'
+            );
+        }
+
+        // Validate input data.
+        $invalid_columns = $this->getInvalidColumns($row);
+        if (empty($invalid_columns)) {
+            throw new InvalidColumnException(
+                'DB#removeRow()',
+                $invalid_columns
+            );
+        }
+
+        // Read each row to determine our results.
+        $all_rows = $this->read();
+        foreach ($all_rows as $index => $value) {
+            // Delete the current row if its diff is empty.
+            if (empty(array_diff($row, $value))) {
+                unset($all_rows[$index]);
+            }
+        }
+
+        // Build new CarafeDB contents to store.
+        $data = '';
+        foreach ($all_rows as $index => $value) {
+            $data .= json_encode($value) . PHP_EOL;
+        }
+
+        // Overwrite the current CarafeDB.
+        file_put_contents($this->db, $data, LOCK_EX);
     }
 }
